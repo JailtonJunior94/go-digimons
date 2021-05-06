de913f67093ceac87b3948d52d9793c30e2f935e
jailton.junior <jailton.junior@iterative.com.br>
2021-05-06 17:06:56 -0300

    Ajustes na leitura de arquivo

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/jailtonjunior94/go-digimons
 
-go 1.15
+go 1.16
--- src/services/digimon_service.go
+++ src/services/digimon_service.go
@@ -1,13 +1,13 @@
 package services
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"path/filepath"
-	"strings"
 	"sync"
 
 	"github.com/jailtonjunior94/go-digimons/src/structs"
@@ -17,16 +17,24 @@ import (
 func GetNames() []string {
 	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-
 	path := filepath.Join(dir, "src", "files", "digimon.txt")
 
-	content, err := ioutil.ReadFile(path)
+	file, err := os.Open(path)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	return strings.Split(string(content), "\n")
+	defer file.Close()
+
+	var digimons []string
+
+	lines := bufio.NewScanner(file)
+	for lines.Scan() {
+		digimons = append(digimons, lines.Text())
+	}
+
+	return digimons
 }
 
 // Request is a function
@@ -35,13 +43,13 @@ func Request(url string, wg *sync.WaitGroup) {
 
 	response, err := http.Get(url)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	defer response.Body.Close()
 
 	if err := json.NewDecoder(response.Body).Decode(&digimons); err != nil {
 		if err != nil {
-			panic(err)
+			log.Fatal(err)
 		}
 	}
 
